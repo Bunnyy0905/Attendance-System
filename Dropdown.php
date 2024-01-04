7dfc724a5090cc67e942df5b20238d36ad0080ff
Ash <126411621+Bunnyy0905@users.noreply.github.com>
2024-01-04 22:46:33 +0530

    Update Dropdown.php

--- Dropdown.php
+++ Dropdown.php
@@ -1,5 +1,25 @@
 <?php
 include('dbconnection.php');
+
+if(isset($_POST['submit'])){
+$academic_year=$_POST['academic_year'];
+$branch=$_POST['branch'];
+$semester=$_POST['semester'];
+$division=$_POST['division'];
+$subject=$_POST['subject'];
+$date_from=$_POST['date_from'];
+$date_to=$_POST['date_to'];
+
+$query=mysqli_query($con, "Insert into student_info (academic_year, branch, sem, division, attendance_from, attendance_to) Values ('$academic_year','$branch','$semester','$division','$subject','$date_from','$date_to')");
+if($query){
+    echo "<script>alert('done')</script>";
+
+}else{
+     echo "<script>alert('error')</script>";
+}
+
+
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -9,9 +29,9 @@ include('dbconnection.php');
     <title>Document</title>
 </head>
 <body>
-    <form>
+    <form method="POST">
         <label>Academic Year:</label>
-        <select>
+        <select name="academic_year">
             <?php 
             $acad_year=mysqli_query($con, "Select * from academic_year");
             while($c=mysqli_fetch_array($acad_year)){
@@ -20,9 +40,8 @@ include('dbconnection.php');
 <?php } ?>        
 </select>
 
-
 <label>Branch:</label>
-        <select>
+        <select name="branch">
             <?php 
             $branches=mysqli_query($con, "Select * from branch");
             while($c=mysqli_fetch_array($branches)){
@@ -32,7 +51,7 @@ include('dbconnection.php');
 </select>
 
 <label>Semester:</label>
-        <select>
+        <select name="semester">
             <?php 
             $semester=mysqli_query($con, "Select * from sem");
             while($c=mysqli_fetch_array($semester)){
@@ -42,7 +61,7 @@ include('dbconnection.php');
 </select>
 
 <label>Division:</label>
-        <select>
+        <select name="division">
             <?php 
             $divs=mysqli_query($con, "Select * from division");
             while($c=mysqli_fetch_array($divs)){
@@ -50,8 +69,25 @@ include('dbconnection.php');
 <option value="<?php echo$c['ID']?>"><?php echo $c['division']?></option>
 <?php } ?>        
 </select>
-<button type="submit" name="submit">Submit</button>
+<br><br>
+<label>Subject:</label>
+        <select name="subject">
+            <?php 
+            $sub=mysqli_query($con, "Select * from subject");
+            while($c=mysqli_fetch_array($sub)){
+                ?>
+<option value="<?php echo$c['ID']?>"><?php echo $c['subject']?></option>
+<?php } ?>        
+</select>
 
+<label>From:</label>
+<input type="date" id="date_from" name="date_from">
+
+<label>To:</label>
+<input type="date" id="date_to" name="date_to">
+
+<br><br>
+<button type="submit" name="submit">Submit</button>
     </form>
 </body>
 </html>
